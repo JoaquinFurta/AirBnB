dfd7f1b302c68732fc6286d51e49d41661101aec
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-26 14:15:15 -0800

    hu

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""write a class BaseModel that defines all common
+attributes/methos for other classes"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel class"""
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.id = kwargs["id"]
+            self.created_at = kwargs["created_at"]
+            self.updated_at = kwargs["updated_at"]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dic = self.__dict__.copy()
+        dic.update(
+            {"created_at": self.created_at.isoformat(),
+             "updated_at": self.updated_at.isoformat(),
+             "__class__": __class__.__name__})
+        return dic
