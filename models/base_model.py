54b60bb39b1c9f607571a1542475381664f5f84e
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-27 08:54:10 -0800

    task_5

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 attributes/methos for other classes"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -10,24 +11,28 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if kwargs:
+            time = "%Y-%m-%dT%H:%M:%S.%f"
             if "id" in kwargs:
                 self.id = kwargs["id"]
             if "created_at" in kwargs:
-                self.created_at = kwargs["created_at"]
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
             if "updated_at" in kwargs:
-                self.updated_at = kwargs["updated_at"]
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
+
         dic = self.__dict__.copy()
         dic.update(
             {"created_at": self.created_at.isoformat(),
---
+++ models/engine/file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import json
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects.update({f"{obj.__class__.__name__}.{obj.id}": obj})
+
+    def save(self):
+        dic_f = {key: value.to_dict() for key, value in self.__objects.items()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(dic_f))
+
+    def reload(self):
+        from models.base_model import BaseModel
+
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                dict_j = json.loads(f.read())
+            for key, value in dict_j.items():
+                self.__objects[key] = BaseModel(**value)
+        except FileNotFoundError:
+            pass
