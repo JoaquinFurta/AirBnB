7bdcbc28691fdedbc31f3e2b68af321656c9e459
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-27 09:46:45 -0800

    consoloe

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt ="(hbnb) "
+
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """End of file"""
+        print()
+        return True
+
+    def emptyline(self):
+        """emptyline"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
